922bdeec247ee8e530683817ecaa267e141be529
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-01-29 16:09:12 +0530

    Node js Architecture

--- modules/file.js
+++ modules/file.js
@@ -1,21 +1,15 @@
 const { isUtf8 } = require("buffer")
 const fs= require("fs")
-// sync 
-// fs.writeFileSync("./ test.txt", "modules");
-// // Async
-// fs.writeFile("./test.txt", "modules Async",(err)=>{})
-// const result=fs.readFileSync("./contact.txt","utf-8");
-// console.log(result)
+const os = require("os")
 
-// fs.readFile("./contact.txt","utf-8",(err,result) => {
-//     if(err){
-//         console.log("Error",err);
-//     }else{
-//         console.log(result); 
-//     }
-// });
-fs.appendFileSync("./test.txt",`${Date.now()}Hey there\n`);
-// fs.cpSync("./test.txt","./copy.txt")
-// fs.unlinkSync("./copy.txt")
-console.log(fs.statSync("./test.txt"));
-fs.mkdirSync("my-docs")
+console.log(os.cpus().length)
+//console.log("1")
+// //sync blocking request
+// const result=fs.readFileSync("contact.txt", "utf-8");
+// console.log(result);
+// console.log("2")
+// Async non blocking request
+fs.readFile("contact.txt", "utf-8",(err, result)=>{
+    //console.log(result)
+})
+//console.log("2")
