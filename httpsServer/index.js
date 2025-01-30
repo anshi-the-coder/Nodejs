7e07537bbf4fdbc515dcee0d73a2a1b7bde00f18
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-01-30 12:52:04 +0530

    Handling URL's in nodejs

--- httpsServer/index.js
+++ httpsServer/index.js
@@ -1,16 +1,24 @@
 const http= require("http"); // http is a built in module
 const fs= require("fs")
+const url= require("url")
 
 const myServer = http.createServer((req, res)=>{     // it takes callback function which can process my incoming request
+    if(req.url==="/favicon.ico") return res.end();
     const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl)
     fs.appendFile("log.txt", log, (err, data)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
             case '/': 
             res.end("HomePage")
             break
             case '/about':
-                 res.end("I am Anshika Gangwar")
+                const username= myUrl.query.myname;
+                 res.end(`Hi, ${username}`)
             break
+            case '/search':
+            const search= myUrl.query.search_query
+            res.end("Here are your result for"+ search)
             default:
                 res.end("404 Not Found")
         }
