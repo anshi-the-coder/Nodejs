b6fb477c171b913b0b11ba77f98ec457e6430ee9
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-02-05 17:33:59 +0530

    HTTP Status Codes

--- Project_01/index.js
+++ Project_01/index.js
@@ -31,7 +31,7 @@ app.get("/users", (req, res) => {
 
 // Rest API
 app.get("/api/users", (req, res) => {
-  res.setHeader("X-MyName", "Anshika Gangwar") // custom header
+  res.setHeader("X-MyName", "Anshika Gangwar"); // custom header
   // Always add X to custum headers
   return res.json(users);
 });
@@ -41,6 +41,7 @@ app
   .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if(!user) return res.status(404).json({error: "user not found"})
     return res.json(user);
   })
   .patch((req, res) => {
@@ -53,9 +54,18 @@ app
   });
 app.post("/api/users", (req, res) => {
   const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  )
+    return res.status(400).json({ msg: "All fields are req..." });
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "Complete" });
+    return res.status(201).json({ status: "Success", id: users.length });
   });
 });
 
