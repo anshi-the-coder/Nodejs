fe72e6653cf0c673215ecc47f6317f8b5b0cc724
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-02-05 13:36:10 +0530

    Express Middleware

--- Project_01/index.js
+++ Project_01/index.js
@@ -1,49 +1,60 @@
-const express = require("express"); 
-const fs= require('fs');
+const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
 
 // Middleware-Plugin
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  fs.appendFile(
+    "log.txt",
+    `${Date.now()}: ${req.ip} ${req.method}: ${req.path}\n`,
+    (err, data) => {
+      next();
+    }
+  );
+});
 
 // Routes
 
-app.get('/users', (req,res)=>{
+app.get("/users", (req, res) => {
   const html = `
   <ul>
-  ${users.map(user =>`<li>${user.first_name}</li>`).join("")}
+  ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
   </ul>
-  `
-  res.send(html)
-})
+  `;
+  res.send(html);
+});
 
 // Rest API
-app.get("/api/users",(req,res)=>{
-  return res.json(users)
-})
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
 
 app
-.route('/api/users/:id').get((req,res)=>{
- const id= Number(req.params.id);
- const user= users.find((user)=> user.id===id)
- return res.json(user)
-})
-.patch((req,res)=>{
-  // Edit user with id
- res.json({status: "Pending"})
-})
-.delete((req,res)=>{
-  // Delete user with id
-  res.json({status: "Pending"})})
-app.post('/api/users', (req,res)=>{
-  const body = req.body
-  users.push({...body, id: users.length+1})
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err, data)=> {
-    return res.json({status:"Complete"})
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
   })
-})
+  .patch((req, res) => {
+    // Edit user with id
+    res.json({ status: "Pending" });
+  })
+  .delete((req, res) => {
+    // Delete user with id
+    res.json({ status: "Pending" });
+  });
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Complete" });
+  });
+});
 
-
-app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`))
+app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
